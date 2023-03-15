d4408b9fb85de9a10bc3fda631ec923f75ebb746
Riya Bulia <riyabulia12@gmail.com>
2023-03-15 15:00:38 +0530

    Update printLinkedList.js

--- printLinkedList.js
+++ printLinkedList.js
@@ -1,36 +1,36 @@
-This challenge is part of a MyCodeSchool tutorial track and is accompanied by a video lesson.
-This is an to practice traversing a linked list. Given a pointer to the head node of a linked list, print each node's  element, one per line. If the head pointer is null (indicating the list is empty), there is nothing to print.
+// This challenge is part of a MyCodeSchool tutorial track and is accompanied by a video lesson.
+// This is an to practice traversing a linked list. Given a pointer to the head node of a linked list, print each node's  element, one per line. If the head pointer is null (indicating the list is empty), there is nothing to print.
 
-Function Description
+// Function Description
 
-Complete the printLinkedList function in the editor below.
+// Complete the printLinkedList function in the editor below.
 
-printLinkedList has the following parameter(s):
+// printLinkedList has the following parameter(s):
 
-SinglyLinkedListNode head: a reference to the head of the list
-Print
+// SinglyLinkedListNode head: a reference to the head of the list
+// Print
 
-For each node, print its  value on a new line (console.log in Javascript).
-Input Format
+// For each node, print its  value on a new line (console.log in Javascript).
+// Input Format
 
-The first line of input contains , the number of elements in the linked list.
-The next  lines contain one element each, the  values for each node.
+// The first line of input contains , the number of elements in the linked list.
+// The next  lines contain one element each, the  values for each node.
 
-Note: Do not read any input from stdin/console. Complete the printLinkedList function in the editor below.
+// Note: Do not read any input from stdin/console. Complete the printLinkedList function in the editor below.
 
-Constraints, where  is the  element of the linked list.
-Sample Input
+// Constraints, where  is the  element of the linked list.
+// Sample Input
 
-2
-16
-13
-Sample Output
+// 2
+// 16
+// 13
+// Sample Output
 
-16
-13
-Explanation
+// 16
+// 13
+// Explanation
 
-There are two elements in the linked list. They are represented as 16 -> 13 -> NULL. So, the printLinkedList function should print 16 and 13 each on a new line.
+// There are two elements in the linked list. They are represented as 16 -> 13 -> NULL. So, the printLinkedList function should print 16 and 13 each on a new line.
 
 'use strict';
 
